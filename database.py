7c46bb6cb51e04caba10331e5918c14a10de489e
amramron28-hash <amramron28@gmail.com>
2026-06-08 14:31:36 +0100

    Update database.py

--- database.py
+++ database.py
@@ -13,3 +13,6 @@ def save_db(data):
     with open('models_db.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+def add_notification(message):
+    # دالة فارغة لإرضاء السكربت ومنع خطأ الاستيراد
+    pass
