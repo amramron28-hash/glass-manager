4756f1e98240683e28ae9ae72d3abf34992ddd1c
amramron28-hash <amramron28@gmail.com>
2026-06-25 19:05:15 +0100

    Update service-worker.js

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,1 +1,110 @@
-const CACHE_NAME="glass-manager-cache-v1",urlsToCache=["./","./manifest.json","./models_db.json"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(e=>e.addAll(urlsToCache)))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request).catch(()=>caches.match("./"))))});
+const CACHE_NAME = "glass-manager-cache-v2";
+
+const STATIC_ASSETS = [
+    "./",
+    "./manifest.json",
+    "./models_db.json"
+];
+
+self.addEventListener("install", (event) => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(STATIC_ASSETS);
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+
+    );
+
+});
+
+self.addEventListener("activate", (event) => {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then((cacheNames) => {
+
+                return Promise.all(
+
+                    cacheNames.map((cacheName) => {
+
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
+
+                    })
+
+                );
+
+            })
+            .then(() => {
+                return self.clients.claim();
+            })
+
+    );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then((cachedResponse) => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+
+                    .then((networkResponse) => {
+
+                        if (
+                            !networkResponse ||
+                            networkResponse.status !== 200 ||
+                            networkResponse.type !== "basic"
+                        ) {
+                            return networkResponse;
+                        }
+
+                        const responseClone = networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseClone);
+                            });
+
+                        return networkResponse;
+
+                    });
+
+            })
+
+            .catch(() => {
+
+                return caches.match("./");
+
+            })
+
+    );
+
+});
+
+self.addEventListener("message", (event) => {
+
+    if (event.data === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+
+});
