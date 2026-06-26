f2b82809cad27bd7b7114cc9438ea3294c98c406
amramron28-hash <amramron28@gmail.com>
2026-06-27 00:20:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
+import shiny
 
 print("RUNNING:", os.path.abspath(__file__))
+print("Shiny version:", shiny.__version__)
 
 from shiny import App
 
 from ui_components import app_ui
 from server import server
-
 
 print("BEFORE APP CREATE")
 
