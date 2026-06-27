11caccb6121159ee9e4a8e15112831ae1ddab49d
amramron28-hash <amramron28@gmail.com>
2026-06-27 13:43:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,25 @@ app = App(
 )
 
 print("APP CREATED")
+import os
+import shiny
+
+print("RUNNING:", os.path.abspath(__file__))
+print("CURRENT DIR:", os.getcwd())
+print("WWW EXISTS:", os.path.isdir("www"))
+print("WWW FILES:", os.listdir("www") if os.path.isdir("www") else "NOT FOUND")
+print("Shiny version:", shiny.__version__)
+
+from shiny import App
+
+from ui_components import app_ui
+from server import server
+
+print("BEFORE APP CREATE")
+
+app = App(
+    app_ui,
+    server
+)
+
+print("APP CREATED")
