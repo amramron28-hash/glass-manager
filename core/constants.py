a75bb064e56ae742bb1cc796c7000de5540ecb2c
amramron28-hash <amramron28@gmail.com>
2026-06-29 23:46:49 +0100

    Create constants.py

---
+++ core/constants.py
@@ -0,0 +1,34 @@
+"""
+ثوابت التطبيق الموحدة
+"""
+
+# إعدادات النظام
+MODELS_INDEX_FILE = "models_index.txt"
+MAX_SUGGESTIONS = 10
+REFRESH_INTERVAL_SEC = 5
+DEBOUNCE_MS = 250
+
+# ألوان الواجهة
+COLORS = {
+    "exact": "#2ecc71",
+    "plus": "#3498db",
+    "minus": "#e67e22",
+    "foundation": "#9b59b6",
+    "plan2_btn": "#00bfff",
+    "plan3_btn": "#e67e22",
+}
+
+# معرفات العناصر
+IDS = {
+    "search_query": "search_query",
+    "selected_trigger": "selected_model_trigger",
+    "close_drawer_trigger": "btn_close_drawer_trigger",
+    "btn_settings": "btn_settings",
+    "p2_search": "p2_search",
+    "p3_search": "p3_search",
+    "merge_p2": "btn_learn_and_merge",
+    "merge_p3": "btn_learn_and_merge_p3",
+    "foundation": "btn_foundation",
+    "trigger_p2": "trigger_plan_2",
+    "trigger_p3": "trigger_plan_3",
+}
